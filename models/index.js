21794fb77434a8755bfad5417841a3f5d0998a6f
aaansari123 <asadali1029@gmail.com>
2022-08-27 22:59:32 -0400

    added associations

--- models/index.js
+++ models/index.js
@@ -5,13 +5,31 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Products.belongsto(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
+Category.hasMany(Products, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: 'products_tags'
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: 'tags_products'
+});
 module.exports = {
   Product,
   Category,
